5265ea3d09a6bc126f889ff859e2f3c1db76c382
Asura146 <kyo008g@gmail.com>
2025-05-05 02:27:50 +0900

    ミドルウェアのコードをコメントアウトして無効化

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,15 @@
-export { auth as middleware } from "./auth";
+// import { NextResponse, NextRequest } from "next/server";
+// import { getToken } from "next-auth/jwt";
+// import { auth } from "./auth";
 
-export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)']
-}
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
+// };
+
+// export async function middleware(req: NextRequest) {
+//     const session = await auth()
+//   if (!session ) {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
+//   return NextResponse.next();
+// }
