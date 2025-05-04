319ec27cc721f09e56e3e5aa6f45d86cec03ef83
Asura146 <kyo008g@gmail.com>
2025-05-05 02:26:58 +0900

    サインイン関数に認証チェックを追加し、認証済みユーザーをトップページにリダイレクトするように修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 
-import { signIn } from "../../auth"
+import { auth, signIn } from "../../auth"
+import { redirect } from "next/navigation"
  
-export default function SignIn() {
+export default async function SignIn() {
+  // ログインしているかどうかを確認
+  const session = await auth();
+  if (session) {
+    // If the user is already authenticated, redirect them to the top page
+    return redirect("/top");
+  }
+
   return (
     <form
       action={async () => {
