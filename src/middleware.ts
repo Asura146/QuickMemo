ad73f4089e546641ef225afa701b6624f243f0d9
Asura146 <kyo008g@gmail.com>
2025-05-20 02:26:59 +0900

    middlewareを適切なディレクトリに移動し、matcherの追加とauth関数のエクスポートをした

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+// import { NextResponse, NextRequest } from "next/server";
+// import { getToken } from "next-auth/jwt";
+import { auth } from "../auth";
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)"],
+};
+export { auth as middleware } from "../auth"
+
+// export async function middleware(req: NextRequest) {
+//     const session = await auth()
+//   if (!session ) {
+//     return NextResponse.redirect(new URL("/", req.url));
+//   }
+//   return NextResponse.next();
+// }
+
+
+// export default auth((req) => {
+//     if (!req.auth && req.nextUrl.pathname !== "/") {
+//       const newUrl = new URL("/", req.nextUrl.origin)
+//       return Response.redirect(newUrl)
+//     }
+//   })
