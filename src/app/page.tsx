867c3b99318a54d094be90b5cb5ab169d7fa5702
Asura146 <kyo008g@gmail.com>
2025-06-11 12:46:45 +0900

    ログインフォームのカードコンポーネントのサイズを調整し、ボタンのスタイルを改善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function SignIn() {
   return (
     <>
       <div className="w-full h-screen bg-white dark:bg-black flex items-center justify-center">
-        <Card className="w-100 h-100 dark:bg-gray-800 bg-red-400 flex flex-col p-4">
+        <Card className="w-90 h-90 dark:bg-gray-800 bg-red-400 flex flex-col p-4">
           <h2 className="text-2xl font-bold  text-center">ログイン</h2>
           <div className="flex-1 flex items-center justify-center">
             <form
@@ -26,8 +26,8 @@ export default async function SignIn() {
               }}
               className="p-4 flex items-center justify-center"
             >
-              <Button type="submit">
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="15" height="15">
+              <Button type="submit" className="w-60 h-15 text-md">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="20" height="20">
                   <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
                   <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
                   <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
