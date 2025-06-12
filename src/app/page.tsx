cbaef6fe9c2c43663a939cd81941477dd7c14fd0
Asura146 <kyo008g@gmail.com>
2025-06-12 14:39:05 +0900

    ログインフォームの背景色を変更し、カードコンポーネントにDividerを追加してデザインを改善

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { auth, signIn } from "../../auth"
 import { redirect } from "next/navigation"
 import { Button } from "@heroui/button"
 import { Card, CardHeader, CardBody} from "@heroui/card"
+import { Divider } from "@heroui/react"
 import  ThemeSwitcher  from "@/components/ThemeSwitcher";
 import { px } from "framer-motion";
  
@@ -15,9 +16,10 @@ export default async function SignIn() {
 
   return (
     <>
-      <div className="w-full h-screen bg-white dark:bg-black flex items-center justify-center">
-        <Card className="w-90 h-90 dark:bg-gray-800 bg-red-400 flex flex-col p-4">
-          <h2 className="text-2xl font-bold  text-center">ログイン</h2>
+      <div className="w-full h-screen bg-gray-100 dark:bg-gray-900 flex items-center justify-center">
+        <Card className="w-90 h-90 dark:bg-gray-950 bg-white flex flex-col p-4">
+          <h2 className="text-2xl font-bold  text-center text-black dark:text-white">ログイン</h2>
+          <Divider className="my-4" />
           <div className="flex-1 flex items-center justify-center">
             <form
               action={async () => {
@@ -26,7 +28,7 @@ export default async function SignIn() {
               }}
               className="p-4 flex items-center justify-center"
             >
-              <Button type="submit" className="w-60 h-15 text-md">
+              <Button type="submit" className="w-60 h-15 text-md ">
                 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="20" height="20">
                   <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
                   <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
